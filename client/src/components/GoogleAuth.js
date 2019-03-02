4360b61d4065f34a32a055b6e60fce6421bc6fbd
UchuNoKen <xen.o.fite@gmail.com>
2019-03-02 17:44:55 -0500

    record the userId

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: "202832029194-socb25nhumgo6a5c50qmcdtjas1v3c6n.apps.googleusercontent.com",
+          scope: "email"
+        })
+        // when library finishes initializing
+        .then(() => {
+          // assign auth instance to this.auth
+          this.auth = window.gapi.auth2.getAuthInstance();
+          // update inside redux store
+          this.onAuthChange(this.auth.isSignedIn.get());
+          // listen for auth status to change
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(
+  mapStateToProps,
+  { signIn, signOut }
+)(GoogleAuth);
