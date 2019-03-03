11f19e9e695f799eb69c856a24737bc3c9932247
UchuNoKen <xen.o.fite@gmail.com>
2019-03-03 17:16:24 -0500

    customize form fields

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -5,7 +5,7 @@ class StreamCreate extends React.Component {
   renderInput({ input, label }) {
     return (
       <div className="field">
-        <label />
+        <label>{label}</label>
         <input {...input} />
       </div>
     );
@@ -13,7 +13,7 @@ class StreamCreate extends React.Component {
 
   render() {
     return (
-      <form>
+      <form className="ui form">
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
       </form>
