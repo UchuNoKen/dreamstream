c355c8d58dbb1362590dbad6df5b42d067b3781e
UchuNoKen <xen.o.fite@gmail.com>
2019-05-08 19:03:12 -0400

    implement delete stream in reducer install lodash

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import {
   FETCH_STREAM,
   FETCH_STREAMS,
@@ -17,7 +18,7 @@ export default (state = {}, action) => {
     case DELETE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
-      return { ...state, [action.payload.id]: action.payload };
+      return _.omit(state, action.payload);
     default:
       return state;
   }
