e0acf46e3ecae29b590cda5636c20966ac7205a6
UchuNoKen <xen.o.fite@gmail.com>
2019-05-10 11:41:16 -0400

    create custom browser history object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
@@ -10,7 +10,7 @@ import Header from "./Header";
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -19,7 +19,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
